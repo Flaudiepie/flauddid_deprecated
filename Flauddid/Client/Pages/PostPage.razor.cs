9eeed3d39c1cbd503b2a0e29b86d7aa589afd121
Florian Böse <florian@boese.ovh>
2021-06-28 22:23:14 +0200

    Post Page Layout implementiert

--- Flauddid/Client/Pages/PostPage.razor.cs
+++ Flauddid/Client/Pages/PostPage.razor.cs
@@ -15,14 +15,36 @@ namespace Flauddid.Client.Pages
         [Parameter]
         public string ID { get; set; }
         private Domain.Entities.Post Post { get; set; }
+        private string Votes { get; set; }
+
         [Inject]
         public IPostService postService { get; set; }
 
         protected async override Task OnInitializedAsync()
         {
             Post = await postService.GetAsync(new PostInfo(SubReddit, ID));
-
+            if (Post.UpVotes > 999)
+            {
+                Votes = Math.Round((double)(Post.UpVotes / 1000), 1).ToString();
+                Votes += "k";
+            }
+            else
+            {
+                Votes = Post.UpVotes.ToString();
+            }
             await base.OnInitializedAsync();
+        }
+
+        protected void PostUpVote()
+        {
+            Post.VoteState = Post.VoteState == VoteState.Up ? VoteState.Neutral : VoteState.Up;
+            postService.UpdateAsync(Post);
+        }
+
+        protected void PostDownVote()
+        {
+            Post.VoteState = Post.VoteState == VoteState.Down ? VoteState.Neutral : VoteState.Down;
+            postService.UpdateAsync(Post);
         }
     }
 }
